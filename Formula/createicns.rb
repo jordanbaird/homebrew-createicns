94ac49113c0c10ac6e3b716886fd80f387e64211
Jordan Baird <jordanbaird.dev@gmail.com>
2022-10-03 02:01:48 -0600

    Point to version 0.0.2

--- Formula/createicns.rb
+++ Formula/createicns.rb
@@ -1,8 +1,8 @@
 class Createicns < Formula
   desc "Create an icns file from any image."
   homepage "https://github.com/jordanbaird/createicns"
-  url "https://github.com/jordanbaird/createicns/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "a188a066c66f872a143d771005941200f39cd1802cde5fb9a8232d94f3cb4490"
+  url "https://github.com/jordanbaird/createicns/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "309228b69107fd33219a8272a8957c0a5c545f5da194ed1b669be3284bae4d8e"
   license "MIT"
   
   def install
