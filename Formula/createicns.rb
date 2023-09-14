3564a683c4c52842eb4e7ad0a848bcf00002db78
Jordan Baird <jordanbaird.dev@gmail.com>
2023-09-14 04:19:19 -0600

    Point version to 0.1.0
    Use swift build to install tool instead of prebuilt binary

--- Formula/createicns.rb
+++ Formula/createicns.rb
@@ -1,11 +1,14 @@
 class Createicns < Formula
   desc "Create an icns file from any image."
   homepage "https://github.com/jordanbaird/createicns"
-  url "https://github.com/jordanbaird/createicns/archive/refs/tags/0.0.4.tar.gz"
-  sha256 "73511bf86f878bc3aba5693ee9d144829e4a2c1f9837719cfd8908b189c46864"
+  url "https://github.com/jordanbaird/createicns/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "7c599d3e53ebb41d2d11e80e5ae063b012ef0c81a323d402b836ccfb314902b3"
   license "MIT"
   
   def install
-    bin.install "createicns"
+    system "swift", "build",
+        "--configuration", "release",
+        "--disable-sandbox"
+    bin.install '.build/release/createicns'
   end
 end
